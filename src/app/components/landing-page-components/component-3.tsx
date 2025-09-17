d4d97079757c0a088b0dbfd0dda98aa0fb98cc87
Sarthak Shrestha <sarthakstha24@gmail.com>
2025-09-17 12:25:06 +0545

    Updated src

--- src/app/components/landing-page-components/component-3.tsx
+++ src/app/components/landing-page-components/component-3.tsx
@@ -58,7 +58,7 @@ export default function Component3() {
         <div className="w-full md:w-[700px] md:h-[500px] flex items-center justify-center">
           <div className="w-full aspect-square md:h-full relative">
             <img
-              src="/images/component3.png"
+              src="/images/Component3.png"
               alt={t("restaurant_image_alt")}
               className="object-cover rounded w-full h-full"
             />
