03b114a36bf20f4ec88dbe1d328f17ea75039c4a
Sarthak Shrestha <sarthakstha24@gmail.com>
2025-09-22 15:47:12 +0545

    Added google site verification

---
+++ src/app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <meta
+        name="google-site-verification"
+        content="yvxxzA1F7RGvGhZUn9Eh-VdeSzAcfxrrHsd1NEvR7hw"
+      />
+    </>
+  );
+}
