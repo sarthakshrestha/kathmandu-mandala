40befac747f59d5a7385c9815a2a55c771441998
Sarthak Shrestha <sarthakstha24@gmail.com>
2025-09-22 13:59:22 +0545

    Removed asset prefix

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,6 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["placehold.co"],
   },
-  assetPrefix:
-    process.env.NODE_ENV === "production"
-      ? "https://www.kathmandumandala.de"
-      : "",
 };
 
 export default nextConfig;
