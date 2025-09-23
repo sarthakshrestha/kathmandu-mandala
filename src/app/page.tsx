127ed8741cd098d13fbfca3271477c59c367262d
Sarthak Shrestha <sarthakstha24@gmail.com>
2025-09-23 17:36:33 +0545

    added seo for dev

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 import LandingPage from "./pages/landing-page";
+import { useSeo } from "./hooks/use-seo";
 
 export default function Home() {
+  useSeo("HOME");
   return <LandingPage />;
 }
